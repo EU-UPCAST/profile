d16477e92d2beeb3fb5216e31db53c9d113bdc2e
Shanshan Jiang <shanshan.jiang@sintef.no>
2024-03-13 08:47:45 +0100

    Updated for the demo, changed api path

--- fastapi_demo.py
+++ fastapi_demo.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException, Path
 from fastapi.responses import FileResponse, JSONResponse
 from enum import Enum
 import os
@@ -6,6 +6,8 @@ import logging
 import uuid
 import pandas as pd
 from ydata_profiling import ProfileReport
+from typing import Optional
+import requests
 
 app = FastAPI()
 
@@ -41,9 +43,8 @@ def run_App1(file: UploadFile):
     #jsonfile = profile.to_file(json_profile)
 
     jsonfile = profile.to_file(output_file_path)
-    output_filename = json_profile
-    output_file_path = output_filename
-
+    output_filename = output_file_path
+ 
     return output_file_path, output_filename
 
 # Function to run App2
@@ -89,22 +90,34 @@ def run_app(selected_app: str, file: UploadFile):
     else:
         raise ValueError("Invalid app name")
 
+# Function for returning the list of available profiles
+def handle_get_profiles(url: str, selected_profiler: str):
+    # for demo purpose, return a statis list of profiles
+    profiles_ydata = ["sample_csv_profile.json", "EV_Population_Data_Sample_profile.json"]
+    profile_list = ["sample_csv_profile.json", "EV_Population_Data_Sample_profile.json", "profile_other_profilers.json"]
 
+    if selected_profiler is not None and selected_profiler == "Ydata":
+        lists = profiles_ydata
+    else:
+        lists = profile_list
+    
+
+    return lists 
 
 # Endpoint to get list of available apps (profilers)
-@app.get("/get_profilers")
+@app.get("/profile/get_profilers")
 async def get_available_profilers():
     return [app_name.value for app_name in AppName]
 
 # Endpoint to generate profile using the selected app
-@app.post("/generate_profile")
-async def generate_profile_with_selected_profiler(selected_profiler: str = Form(...), file: UploadFile = File(...)):
-    logger.info(f"Request Payload: selected_app={selected_profiler}, file={file.filename}")
+@app.post("/profile/generate_profile")
+async def generate_profile_for_local_file_with_selected_profiler(selected_app: str = Form(...), file: UploadFile = File(...)):
+    logger.debug(f"Request Payload: selected_app={selected_app}, file={file.filename}")
 
     if selected_app not in [app_name.value for app_name in AppName]:
         return JSONResponse(content={"error": "Invalid app name"}, status_code=400)
     
-    output_file_path, output_filename = run_app(selected_profiler, file)
+    output_file_path, output_filename = run_app(selected_app, file)
     
     # Read content of the output file
     with open(output_file_path, "r") as output_file:
@@ -113,13 +126,123 @@ async def generate_profile_with_selected_profiler(selected_profiler: str = Form(
     # Return the filename and content of the output file
     return JSONResponse(content={"filename": output_filename, "file_content": file_content})
 
+# retrive profile of a resource specified by url, optionally with the profiler used
+@app.get("/profile/get_profile")
+async def get_profile_from_url(url: str, profiler_name: Optional[str] = None):
+
+    try:
+        logger.info(f"Received URL: {url}")
+        if url.startswith(("http://", "https://")):
+            logger.info(f"This is a remote url")
+        
+            # Fetch file content from the URL
+            #response = requests.get(url)
+            #response.raise_for_status()  # Raise exception for any HTTP error
+
+            # call the profiler to generate the profile OR return the profile with specified profiler
+            # TODO: this code need to be updated according to the local configuration. 
+            # TODO: case 1) the profile exists: simply return the profile for the resource specified
+            # TODO: case 2) the profile does not exist: call the specified profiler to generate the profile
+
+            # Return the content of the file in the response
+            #return response.content
+
+            # Call the function that handle this request
+            return JSONResponse(content={"profiles": handle_get_profiles(url, profiler_name)})
+
+           
+        else:
+            # this is a local file, mostly for demo purpose
+            logger.info(f"This is a local file: {url}")
+            if os.path.exists(url):
+                # TODO: # call the profiler to generate the profile OR return the profile with specified profiler
+                #with open(url, "rb") as file:
+                #    return file.read()
+                # Call the function that handle this request
+                return JSONResponse(content={"profiles": handle_get_profiles(url, profiler_name)})
+            else:
+                print(f"File not found at local path: {url}")
+                return None
+
+    except Exception as e:
+        return {"error": f"Failed to fetch file from URL: {url}. Error: {str(e)}"}
+
+
+# Endpoint to update a profile at the specified URL with content from a local file
+@app.put("/profile/update_profile")
+async def update_profile_specified_by_url_with_local_file(url: str, file_content: UploadFile = File(..., description="Local file to update with")
+):
+    try:
+        
+        if url.startswith(("http://", "https://")):
+            # TODO: update the remote url with the uploaded file
+            #return JSONResponse(content={"message": f"File at '{url}' is a remote file"})
+            # Use PUT request to update remote file
+            response = requests.put(url, data=await file_content.read())
+            if response.status_code == 200:
+                return {"message": f"Remote file at '{url}' updated successfully"}
+            else:
+                raise HTTPException(status_code=response.status_code, detail=f"Failed to update remote file. Response: {response.text}")
+
+
+        else:
+            # This is a local file. Check if the provided URL is valid
+            #url_path = Path(url)
+            if not os.path.exists(url):
+                raise HTTPException(status_code=404, detail="File not found at the specified URL.")
+
+            # Check if a file is provided as content
+            if not file_content:
+                raise HTTPException(status_code=400, detail="No file provided as content.")
+            
+            # Read the content of the uploaded file
+            file_bytes = await file_content.read()
+
+            # Write the content of the new file to the specified URL
+            with open(url, "wb") as f:
+                f.write(file_bytes)
+
+            return JSONResponse(content={"message": f"File at '{url}' updated successfully"})
+    except Exception as e:
+        return JSONResponse(content={"error": f"Failed to update file. Error: {str(e)}"}, status_code=500)
+
+# Endpoint to delete profile specified by URL
+@app.delete("/profile/delete_profile")
+async def delete_profile_specified_by_url(url: str):
+    
+    try:
+        logger.info(f"Received URL: {url}")
+        if url.startswith(("http://", "https://")):
+            # This is a remote URL
+            logger.info(f"handle remote URL, url={url}")
+            print(f"handle remote URL, url={url}")
+            response = requests.delete(url)
+            if response.status_code == 200:
+                return JSONResponse(content={"message": f"File at '{url}' deleted successfully"})
+            else:
+                raise HTTPException(status_code=response.status_code, detail=response.text)
+        else:     
+            # This is a local path. Check if the provided path exists
+            if not os.path.exists(url):
+                raise HTTPException(status_code=404, detail="File not found at the specified URL.")
+
+            # Delete the file
+            os.remove(url)
+
+            return JSONResponse(content={"message": f"File at '{url}' deleted successfully"})
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return JSONResponse(content={"error": f"Failed to delete file. Error: {str(e)}"}, status_code=500)
+
+
+
 # Generate OpenAPI schema
 from fastapi.openapi.utils import get_openapi
 
 openapi_schema = get_openapi(
-    title="UPCAST Data Profiling Service",
+    title="UPCAST Data Profiling API",
     version="0.1.0",
-    description="OpenAPI specification for the UPCAST Data Profiling Service",
+    description="OpenAPI specification for the UPCAST Data Profiling API",
     routes=app.routes,
 )
 
