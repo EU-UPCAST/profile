30bda0606d19b8b8c2ad85bfccd25e7e9198536f
sondsorb <sondresorbo@hotmail.no>
2025-03-06 11:51:13 +0100

    possibility for several inferencce papers

--- llmdap/profiler/run_inference.py
+++ llmdap/profiler/run_inference.py
@@ -29,39 +29,58 @@ def call_inference(
     """
     schema: pydantic form that the pipeline should filll out
 
-    include ONE of the following four string arguments:
+    include ONE of the following four arguments:
         parsed_paper_text: paper text ready for reading
         raw_xml_paper_text: paper text in xml format (typically starting with "<?xml version="1.0" encoding="UTF-8"?>...)"
         paper_path: local path to paper file in XML format
         paper_url: url to paper file in XML format
+    This argument should be either a string, a list of strings, or a dict of strings
     """
 
-    if parsed_paper_text:
-        paper_text = parsed_paper_text
-    elif raw_xml_paper_text:
-        import dataset_loader
-        paper_text = dataset_loader.parse_raw_xml_string(raw_xml_paper_text)
-    elif paper_path:
-        import dataset_loader
-        paper_text = dataset_loader.load_paper_text_from_file_path(paper_path)
-    elif paper_url:
-        import dataset_loader
-        paper_text = dataset_loader.load_paper_text_from_url(paper_url)
-    else:
-        raise ValueError
-
-
+    # prepare arguments
     parameters = add_defaults(kwargs)
-
     args=SimpleNamespace(**parameters)
-
     args.load=False
     args.save=False
     args.schema = schema
 
+    # load stuff
     prepared_kwargs = load_modules(args)
+    ff_iterator = FormFillingIterator(args, **prepared_kwargs)
 
-    return FormFillingIterator(args, **prepared_kwargs).fill_single_form(key="", paper_text=paper_text, pydantic_form=schema, return_dict_with_context=True)
+    # make the argument into dictionary
+    paper_argument = parsed_paper_text or raw_xml_paper_text or paper_path or paper_url
+    if type(paper_argument) is str:
+        paper_argument = {0: paper_argument}
+    elif type(paper_argument) is list:
+        paper_argument = {i:p for i,p in enumerate(paper_argument)}
+    elif type(paper_argument) is dict:
+        pass
+    else:
+        raise ValueError
+    
+    outputs = {}
+    for key in paper_argument:
+        
+        # parse/load/fetch argument (to a string paper ready for the llm)
+        if parsed_paper_text:
+            paper_text = paper_argument[key]
+        elif raw_xml_paper_text:
+            import dataset_loader
+            paper_text = dataset_loader.parse_raw_xml_string(paper_argument[key])
+        elif paper_path:
+            import dataset_loader
+            paper_text = dataset_loader.load_paper_text_from_file_path(paper_argument[key])
+        elif paper_url:
+            import dataset_loader
+            paper_text = dataset_loader.load_paper_text_from_url(paper_argument[key])
+        else:
+            raise ValueError
+
+        # fill form
+        outputs[key] =ff_iterator.fill_single_form(key="", paper_text=paper_text, pydantic_form=schema, return_dict_with_context=True)
+
+    return outputs
 
 
 
@@ -69,24 +88,32 @@ if __name__ == "__main__":
 
     
     path = "/mnt/data/upcast/data/all_xmls/12093373_ascii_pmcoa.xml"
+    path2= "/mnt/data/upcast/data/all_xmls/12095422_ascii_pmcoa.xml"
     import dataset_loader
 
     parsed_xml_paper_text = dataset_loader.load_paper_text_from_file_path(path)
     with open(path, "r") as f:
         raw_xml_paper_text = f.read()
+
     paper_path = path
     paper_url = "https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi/BioC_xml/12093373/ascii"
+    paper_url2= "https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi/BioC_xml/12095422/ascii"
 
 
     from metadata_schemas.arxpr2_schema import Metadata_form as schema
 
     output = call_inference(
             schema,
-            # choose one to try out
+            #
+            # choose one to try out:
+            #
             #parsed_paper_text = parsed_xml_paper_text,
             #raw_xml_paper_text = raw_xml_paper_text,
             #paper_path = paper_path,
-            paper_url = paper_url,
+            #paper_path = [paper_path, path2],
+            #paper_url = paper_url,
+            paper_url = {"paper1": paper_url, "paper2":paper_url2},
+            #
             similarity_k = 5,
             field_info_to_compare = "choices",
             )
